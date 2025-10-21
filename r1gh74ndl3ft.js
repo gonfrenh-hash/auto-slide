bf9de19201febdf3ec193dd94fc714d73e21d7fe
gonfrenh-hash <csgonfren2@gmail.com>
2025-10-21 11:29:30 +0700

    r1gh74ndl3ft.js

--- r1gh74ndl3ft.js
+++ r1gh74ndl3ft.js
@@ -1,24 +1,25 @@
+// Geser otomatis ke kanan lalu balik ke kiri
 (function () {
   const track = document.getElementById("slideTrack");
-  let direction = 1;
+  let direction = 1; // 1 = kanan, -1 = kiri
   let pos = 0;
 
-  const slideWidth = 400;
+  const slideWidth = 400; // sesuai .slide width
   const totalSlides = track.children.length;
-  const maxShift = (totalSlides - 1) * slideWidth;
+  const maxShift = (totalSlides - 1) * slideWidth; // jarak maksimal ke kanan
 
   function animate() {
+    // Gerakan pelan
+    pos += direction * 2; // 2 = kecepatan pixel per frame
 
-    pos += direction * 2;
-
-
+    // Batas kanan
     if (pos >= maxShift) {
-      direction = -1;
+      direction = -1; // balik ke kiri
     }
 
-
+    // Batas kiri
     if (pos <= 0) {
-      direction = 1;
+      direction = 1; // balik ke kanan
     }
 
     track.style.left = -pos + "px";
